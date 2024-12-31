785da8650174087d344ef2b7cc2ee8bd20dc99d6
pradeep <qaguru.update@gmail.com>
2024-12-31 11:10:13 -0500

    Account Test added

---
+++ src/test/java/ca/qaguru/lib/TestBase.java
@@ -0,0 +1,22 @@
+package ca.qaguru.lib;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase {
+    protected final String baseUri = "http://localhost:8080";
+    protected final String basePath = "/api/accounts";
+    protected RequestSpecification requestSpecification;
+    @BeforeSuite
+    public void beforeSuite(){
+        requestSpecification = new RequestSpecBuilder()
+                .log(LogDetail.ALL)
+                .setBaseUri(baseUri)
+                .setBasePath(basePath)
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+}
